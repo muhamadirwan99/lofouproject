6bdd3a17677868b1f051f881d46ca96234598c32
Muhamad Irwan <muhamadirwan99@gmail.com>
2020-05-28 10:52:00 +0700

    mengubah activity_info.xml dan activity_sk.xml

--- app/src/main/java/com/example/lofou/SK.java
+++ app/src/main/java/com/example/lofou/SK.java
@@ -1,6 +1,8 @@
 package com.example.lofou;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class SK extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sk);
+    }
+
+    public void Ok (View view){
+        Intent Ok = new Intent(SK.this, MainActivity.class);
+        startActivity(Ok);
     }
 }
