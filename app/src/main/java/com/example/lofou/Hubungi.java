bb954eef7217a10bea701d4f3ed433e4cb954763
Muhamad Irwan <muhamadirwan99@gmail.com>
2020-05-28 10:31:41 +0700

    mengubah activity_hubungixml, Hubungi.java, Kirim.java, activity_kirim.xml

--- app/src/main/java/com/example/lofou/Hubungi.java
+++ app/src/main/java/com/example/lofou/Hubungi.java
@@ -3,19 +3,38 @@ package com.example.lofou;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Hubungi extends AppCompatActivity {
 
+    EditText text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hubungi);
+
+        text =findViewById(R.id.masalah);
+    }
+
+    public void Batal(View view){
+        Intent Batal = new Intent(Hubungi.this, MainActivity.class);
+        startActivity(Batal);
     }
 
     public void Kirim(View view) {
-        Intent Kirim = new Intent(Hubungi.this, Kirim.class);
-        startActivity(Kirim);
+
+        String teks = text.getText().toString();
+
+        if(!teks.equals("")){
+            Intent Kirim = new Intent(Hubungi.this, Kirim.class);
+            startActivity(Kirim);
+        }else{
+            Toast.makeText(getApplication(), "Tolong isi terlebih dahulu", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
